653a3c7900b7e273dbefb0a8338907af63f7dcc9
lizzy-macdonald <elizabethamac27@gmail.com>
2025-03-19 13:11:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import tensorflow as tf
 # (Ensure that the following files are in your repository)
 # model_selected = keras.models.load_model('model_selected.h5',
 custom_objects={'mse': tf.keras.losses.mean_squared_error}
-# model_all = keras.models.load_model('model_all.h5', custom_objects={'mse':
-tf.keras.losses.mean_squared_error)
+# model_all = keras.models.load_model('model_all.h5', custom_objects={'mse':tf.keras.losses.mean_squared_error})
 model_selected = keras.models.load_model('model_selected.h5')
 model_all = keras.models.load_model('model_all.h5')
 preprocessor_selected = joblib.load('preprocessor_selected.pkl')
